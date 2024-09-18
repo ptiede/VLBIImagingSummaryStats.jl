9cb33ef50e6606fbc8b65e7d77a6e7f1590ce0e7
Paul Tiede <ptiede91@gmail.com>
2024-09-18 17:03:47 -0400

    Harden the setup script

--- scripts/summarystats/setup.jl
+++ scripts/summarystats/setup.jl
@@ -10,9 +10,26 @@ using Pkg; Pkg.activate(@__DIR__)
 using Distributed
 using DelimitedFiles
 
-Pkg.rm("VLBIImagingSummaryStats") #Remove the package if it is in the project otherwise you will get an error
-Pkg.rm("Comrade")
-Pkg.develop(name="VLBIImagingSummaryStats", path="../../")
+try
+    Pkg.rm("Comrade")
+catch e
+
+end
+
+try 
+    Pkg.rm("VLBIImagingSummaryStats")
+catch e
+
+end
+
+if isfile(joinpath(@__DIR__, "Manifest.toml"))
+    @warn "Manifest file detected deleting and creating new one for setup"
+    rm(joinpath(@__DIR__, "Manifest.toml"))
+end
+
+# Pkg.rm("VLBIImagingSummaryStats") #Remove the package if it is in the project otherwise you will get an error
+# Pkg.rm("Comrade")
+Pkg.develop(name="VLBIImagingSummaryStats", url="https://github.com/ptiede/VLBIImagingSummaryStats.jl")
 Pkg.instantiate()
 Pkg.precompile()
 Pkg.add(;name="Comrade", rev="ptiede-enzymeswitch")
